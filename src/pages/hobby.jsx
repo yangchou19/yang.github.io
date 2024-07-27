029bdbe4d3877f7cae141235eda0087aff2b4ce7
yangchou19 <1178574011@qq.com>
2024-07-27 23:45:58 +0800

    finish hobby page

--- src/pages/hobby.jsx
+++ src/pages/hobby.jsx
@@ -2,41 +2,55 @@ import Image from 'next/image'
 import Head from 'next/head'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import huaiyihuai from '@/images/photos/huaiyihuai.png'
-import bellybooklogo from "@/images/photos/bellybook.png"
-import renshengcancan from '@/images/photos/renshengcancan.png'
-import chuntianjianshi from '@/images/photos/chuntianjianshi.webp'
 import Link from 'next/link'
+// logo
+import shootlogo from '@/images/photos/shoot.jpeg'
+import basketballlogo from '@/images/photos/basketball.jpeg'
+import xiatianlogo from '@/images/photos/xiatian.jpeg'
+import skatelogo from '@/images/photos/skate.png'
+import gymlogo from '@/images/photos/gym.jpeg'
+import guitarlogo from '@/images/photos/guitar.png'
 
 const projects = [
   {
-    name: '胃之书App',
+    name: '摄影',
     description:
-      'AI饮食记录员。上线2天突破10000用户。',
-    link: { href: 'https://bellybook.cn', label: '胃之书App官网下载' },
-    logo: bellybooklogo,
+      '摄影是作为记录生活的方式，同时表达关系和情感。从本科开始接触到现在，它让我更加关注生活中忽略的细节，让我思考、平静和记录当下。',
+    link: { href: 'https://carpediemyang.notion.site/Yang-31824db1254648dd884641392516950d', label: '摄影作品记录' },
+    logo: shootlogo,
   },
   {
-    name: '《坏一坏》',
+    name: '篮球',
     description:
-      '小说集。凉炘的小说虽然以“坏一坏”冠名，但其实坏中有深情，我能读到他骨子里藏不住深情款款。 ———蔡崇达。',
-    link: { href: 'https://book.douban.com/subject/26990080/', label: '豆瓣 - 坏一坏 评分8.4' },
-    logo: huaiyihuai,
+      '篮球是我接触最久的运动，小学开始接受训练至今，加入过院队征战比赛，喜欢通过传切的配合后将篮球投进篮筐的过程。',
+    logo: basketballlogo,
   },
   {
-    name: '《人生灿灿》',
+    name: '夏天',
     description:
-      '小说集。收录凉炘短篇小说十四篇，包括在“One·一个”App中高点击量与高赞的作品。',
-    link: { href: 'https://book.douban.com/subject/34865437/', label: '豆瓣 - 人生灿灿 评分7.0' },
-    logo: renshengcancan,
+      '夏天是一只边牧，今年两岁了，头上有个聪明豆，喜欢接球、追光、咬拖把、睡觉；性格上安静、胆小谨慎、有一丢懒。有好多小聪明，会和家里人斗智斗勇，带来了很多快乐。',
+    logo: xiatianlogo,
   },
   {
-    name: '《春天简史》',
+    name: '滑雪',
     description:
-      '编剧/演员',
-    link: { href: 'https://movie.douban.com/subject/35708474/', label: '豆瓣 - 春天简史' },
-    logo: chuntianjianshi,
+      '2024年3月，临近毕业时发现还有学生套餐，去北大湖滑雪，第一次比较长时间体验，北方雪场真的好大，从此喜欢上滑雪，也想着要尝试一些新事物。',
+    link: { href: 'https://carpediemyang.notion.site/6e9aaf154b8d48e0a7351c0933733303', label: '滑雪记录' },
+    logo: skatelogo,
   },
+  {
+    name:'健身',
+    description:
+      '健身是本科开始接触，断断续续坚持很久，虽然没有十分傲人的身材；但体会到一次次击碎自己后又顽强站起来的感觉。其作为平衡工作和生活的方式，让我保持身体健康，短暂离开网络，沉浸在每一个动作中。',
+    link: { href: 'https://carpediemyang.notion.site/84bc500e95c24749b27a0b1123d490d3?pvs=4', label: '健身动作' },
+    logo: gymlogo,
+  },
+  {
+    name: '吉他',
+    description:
+    '高中的时候开始接触吉他，断断续续学了一段时间，但好像发现自己在弹唱上没啥天赋😂，五音不太全。在演奏的过程中，让我心平气和，找到节奏感，而不是急于完成。',
+    logo: guitarlogo,
+  }
 ]
 
 function LinkIcon(props) {
@@ -62,7 +76,7 @@ export default function Projects() {
       </Head>
       <SimpleLayout
         title="探索世界路上，发现的喜欢"
-        intro="永远保持一颗积极热忱的心，对世界充满好奇。"
+        intro="永远保持一颗积极热忱的心，对世界充满好奇，尝试新的事物。"
       >
         <ProjectList />
       </SimpleLayout>
@@ -79,22 +93,29 @@ export function ProjectList() {
     >
       {projects.map((project) => (
         <Card as="li" key={project.name}>
-          <div className="relative z-10 flex h-24 w-24 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+          <div className="relative z-10 flex h-24 w-24 items-center justify-center rounded-lg bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
             <Image
               src={project.logo}
               alt=""
-              className="h-20 w-20 object-cover rounded-full"
+              className="h-20 w-20 object-cover rounded-lg"
               unoptimized
             />
           </div>
           <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-            <Card.Link href={project.link.href}>{project.name}</Card.Link>
+            {project.link
+              ? (<Card.Link href={project.link.href}>{project.name}</Card.Link>)
+              : (<Card.Link>{project.name}</Card.Link>)
+            }
           </h2>
           <Card.Description>{project.description}</Card.Description>
-          <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-            <LinkIcon className="h-6 w-6 flex-none" />
-            <Link href={project.link.href} className="ml-2" target="_blank">{project.link.label}</Link>
-          </p>
+          {
+            project.link && (
+              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <LinkIcon className="h-6 w-6 flex-none" />
+                <Link href={project.link.href} className="ml-2" target="_blank">{project.link.label}</Link>
+              </p>
+            )
+          }
         </Card>
       ))}
     </ul>
